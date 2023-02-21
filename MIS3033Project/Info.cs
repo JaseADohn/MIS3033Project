ebbf167bbd57860ceb44dbd3661d69e16121886d
Jase Dohn <Jasedohn@ou.edu>
2023-02-20 20:38:50 -0600

    oops

---
+++ MIS3033Project/Info.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIS3033Project
+{
+    public class Info
+    {
+        public string Title { get; set; }
+        public string Genre { get; set; }
+
+        public string Director { get; set; }
+
+        public string StarActors { get; set; }
+
+        public string IMBD { get; set; }
+
+        public Info() 
+        {
+            Title = string.Empty;
+            Genre = string.Empty;
+            Director = string.Empty;
+            StarActors = string.Empty;
+            IMBD = string.Empty;
+        }   
+
+    }
+}
--- MIS3033Project/MainWindow.xaml.cs
+++ MIS3033Project/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using System.IO;
 using Microsoft.Win32;
 using Newtonsoft;
 using System.Text.RegularExpressions;
+using Newtonsoft.Json;
 
 namespace MIS3033Project
 {
@@ -43,28 +44,47 @@ namespace MIS3033Project
 
         private void BtnClear_Click(object sender, RoutedEventArgs e)
         {
-            // Open a file dialog to select the input file
             OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "Text files (*.txt)|*.txt|CSV files (*.csv)|*.csv";
+            openFileDialog.Filter = "Text files (*.txt)|*.txt|CSV files (*.csv)|*.csv|JSON files (*.json)|*.json";
             if (openFileDialog.ShowDialog() == true)
             {
                 string inputFilePath = openFileDialog.FileName;
 
                 // Read the input file and clean the data
                 string inputData = File.ReadAllText(inputFilePath);
-                inputData = Regex.Replace(inputData, @"\t|\n|\r", " "); // replace tabs, newlines and carriage returns with spaces
-                inputData = Regex.Replace(inputData, @"\s+", " "); // replace multiple spaces with a single space
-                inputData = inputData.Trim(); // remove leading and trailing spaces
+                string inputExtension = System.IO.Path.GetExtension(inputFilePath);
+
+                if (inputExtension == ".json")
+                {
+                    dynamic json = JsonConvert.DeserializeObject(inputData);
+                    inputData = JsonConvert.SerializeObject(json, Formatting.Indented);
+                }
+                else
+                {
+                    inputData = Regex.Replace(inputData, @"\t|\n|\r", " "); // replace tabs, newlines and carriage returns with spaces
+                    inputData = Regex.Replace(inputData, @"\s+", " "); // replace multiple spaces with a single space
+                    inputData = inputData.Trim(); // remove leading and trailing spaces
+                }
 
                 // Open a save file dialog to select the output file format and location
                 SaveFileDialog saveFileDialog = new SaveFileDialog();
-                saveFileDialog.Filter = "Text files (*.txt)|*.txt|CSV files (*.csv)|*.csv";
+                saveFileDialog.Filter = "Text files (*.txt)|*.txt|CSV files (*.csv)|*.csv|JSON files (*.json)|*.json";
                 if (saveFileDialog.ShowDialog() == true)
                 {
                     string outputFilePath = saveFileDialog.FileName;
+                    string outputExtension = System.IO.Path.GetExtension(outputFilePath);
 
                     // Write the cleaned data to the output file
-                    File.WriteAllText(outputFilePath, inputData);
+                    if (outputExtension == ".json")
+                    {
+                        dynamic json = JsonConvert.DeserializeObject(inputData);
+                        string outputData = JsonConvert.SerializeObject(json, Formatting.Indented);
+                        File.WriteAllText(outputFilePath, outputData);
+                    }
+                    else
+                    {
+                        File.WriteAllText(outputFilePath, inputData);
+                    }
                 }
             }
         }
